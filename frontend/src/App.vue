68de075be95c539ad5230853c5514ea1d8bee439
Chris Sterritt <csterritt@gmail.com>
2022-06-25 12:48:10 -0400

    Stripped down to minimal, unstyled app.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img id="logo" alt="Wails logo" src="./assets/images/logo-universal.png"/>
-  <HelloWorld/>
+  <Search/>
 </template>
 
-<style>
-#logo {
-  display: block;
-  width: 50%;
-  height: 50%;
-  margin: auto;
-  padding: 10% 0 0;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  background-origin: content-box;
-}
-</style>
+<script setup>
+import Search from './components/Search.vue'
+</script>
